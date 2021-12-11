f8964ee67591fb9e31d13def8b29e4345ad4a8d6
tdmitryny <tdmitryny@gmail.com>
2021-12-10 20:33:25 -0500

    Toggle

--- src/components/ToggleButton.js
+++ src/components/ToggleButton.js
@@ -1,10 +1,26 @@
+import { useState } from 'react';
+
+
 
 export function ToggleButton() {
+
+    // const [toggleBtn, setToggleBtn] = useState();
+
+    // const changeBack = () => {
+    //     const html = document.getElementsByTagName('html')
+    //     setToggleBtn((toggle) => {
+    //         toggle.classList.toggle
+    //     })
+
+    // onClick={changeBack}
+    // }
+
     return (
         <div className="toggle">
             <form>
-                <label className="switch">
-                    <input className="slider round" type="checkbox" />
+                <input className="slider round" id="checkbox" type="checkbox" />
+                <label className="switch" for="checkbox">
+                    <div className="ball"></div>
                 </label>
             </form>
         </div>
